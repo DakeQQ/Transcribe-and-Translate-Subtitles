d95077e1f875cc71b8b86254230777ece4c78d6b
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-10-29 22:12:36 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2266,7 +2266,7 @@ def MAIN_PROCESS(
         else:
             init_penality_reset_count_beam = 0
             init_save_id_greedy = np.zeros(MAX_SEQ_LEN_ASR, dtype=np.int32)
-        if asr_type != 0:
+        if asr_type != 0 or device_type_C != 'cpu':
             if device_type_C != 'dml':
                 past_keys_D = onnxruntime.OrtValue.ortvalue_from_numpy(np.zeros((1, ort_session_D._outputs_meta[0].shape[1], ort_session_D._outputs_meta[0].shape[2], 0), dtype=model_D_dtype), device_type_C, DEVICE_ID)
                 past_values_D = onnxruntime.OrtValue.ortvalue_from_numpy(np.zeros((1, ort_session_D._outputs_meta[num_layers].shape[1], 0, ort_session_D._outputs_meta[num_layers].shape[3]), dtype=model_D_dtype), device_type_C, DEVICE_ID)
